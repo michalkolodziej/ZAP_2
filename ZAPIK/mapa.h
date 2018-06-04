4941b29a27ea28fb4392d4bf21a4a8a6936d6699
michalkolodziej <michalkolodziej@tuta.io>
2018-06-04 18:53:44 +0200

    teraz już są okienka, ale nie ma pojedyńczych kroków :c

---
+++ ZAPIK/mapa.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+using  namespace std;
+struct macierz;
+using namespace cv;
+using namespace std;
+
+void **imgtoarray(Mat image, macierz **&tablica);
+
+void showimg(string nazwa, Mat img);
+
+void szukaj(macierz **&mapa, int w, int k);

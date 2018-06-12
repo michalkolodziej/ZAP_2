3ef4bc8fca81aebcf9f92ce3f33267b1fe422b62
michalkolodziej <michalkolodziej@tuta.io>
2018-06-13 00:10:23 +0200

    Obsluga algorytmu

--- ZAPIK/mapa.h
+++ ZAPIK/mapa.h
@@ -1,3 +1,11 @@
+/**
+ * \file mapa.h
+ * \brief Plik nagłówkowy modułu \a mapa.
+ *
+ * Moduł \a mapa zawiera funkcje obsługujące algorytm A*.
+ *
+ * \see mapa.cpp
+ */
 #pragma once
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
@@ -5,13 +13,37 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
-using  namespace std;
+#include "ui_mainwindow.h"
+#include "mainwindow.h"
+#include <QApplication>
 struct macierz;
 using namespace cv;
 using namespace std;
 
-void **imgtoarray(Mat image, macierz **&tablica);
+/** \brief Przekształcenie obrazu w macierz liczb
+ *
+ * Funkjca przekształca obraz podany za pomocą typu Mat na macierz liczb.
+ * \param image obraz do przekształcenia
+ * \param tablica struktura mapy
+ */
+void imgtoarray(Mat image, macierz **&tablica);
 
+/** \brief Pokaż obraz
+ *
+ * Funkjca wypełnia wyświetla okno z obrazem podanym za pomocą typu Mat
+ * \param nazwa nazwa okna
+ * \param img obraz
+ */
 void showimg(string nazwa, Mat img);
 
-void szukaj(macierz **&mapa, int w, int k);
+
+/** \brief Funkcja szukająca najkrótszej drogi na mapie
+ *
+ * Funkcja ma działanie zlbiżone do działania algorytmu A*.
+ * Poszukuje najkrótszej drogi.
+ * \param mapa mapa, która ma zostać przeszukana.
+ * \param w ilość wierszy mapy
+ * \param k ilość kolumn mapy
+ * \param ui przekazanie okna do funkcji
+ */
+void szukaj(macierz **&mapa, int w, int k, Ui::MainWindow *ui);
